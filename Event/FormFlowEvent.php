243deb1865a458a144ae444efcf9e001d9e342b2
Vasily Sokolov <megawilddaddy@gmail.com>
2020-09-04 15:59:37 +0300

    Fix preload

--- Event/FormFlowEvent.php
+++ Event/FormFlowEvent.php
@@ -3,33 +3,15 @@
 namespace Craue\FormFlowBundle\Event;
 
 use Craue\FormFlowBundle\Form\FormFlowInterface;
-use Symfony\Component\EventDispatcher\Event as LegacyEvent;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Contracts\EventDispatcher\Event;
 
-if (Kernel::VERSION_ID < 40300) {
-	// TODO remove as soon as Symfony >= 4.3 is required
-	/**
-	 * @author Christian Raue <christian.raue@gmail.com>
-	 * @copyright 2011-2020 Christian Raue
-	 * @license http://opensource.org/licenses/mit-license.php MIT License
-	 */
-	abstract class FormFlowEvent extends LegacyEvent {
-		use _FormFlowEventTrait;
-	}
-} else {
-	/**
-	 * @author Christian Raue <christian.raue@gmail.com>
-	 * @copyright 2011-2020 Christian Raue
-	 * @license http://opensource.org/licenses/mit-license.php MIT License
-	 */
-	abstract class FormFlowEvent extends Event {
-		use _FormFlowEventTrait;
-	}
-}
-
-trait _FormFlowEventTrait {
-
+/**
+ * @author Christian Raue <christian.raue@gmail.com>
+ * @copyright 2011-2020 Christian Raue
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+abstract class FormFlowEvent extends Event {
 	/**
 	 * @var FormFlowInterface
 	 */
@@ -48,5 +30,4 @@ trait _FormFlowEventTrait {
 	public function getFlow() {
 		return $this->flow;
 	}
-
 }
